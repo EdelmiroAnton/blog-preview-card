c7f2f20f2bc1c50a07d3da20f4d441ad170d707e
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-12-22 20:53:03 +0100

    center card vertically and horizontally

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 function App() {
   return (
-    <>
-      <div className="">
-        VITE AND TAILWIND OK!!
-      </div>
-    </>
+    <div className="w-[327px] h-[501px] border-solid border-2 border-black flex flex-col">
+      <div>img</div>
+      <div>content</div>
+      <div>author</div>
+    </div>
   );
 }
 
