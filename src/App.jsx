6803ca0e1f3f972e778be7c19c74f1864fd20b03
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-12-22 21:06:21 +0100

    add blog image to container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import blog_image from "../src/assets/images/blog_img.svg"
+
 function App() {
   return (
-    <div className="w-[327px] h-[501px] border-solid border-2 border-black flex flex-col">
-      <div>img</div>
+    <main className="w-[327px] h-[501px] border-solid border-2 border-black flex flex-col">
+      <div>
+        <img src={blog_image} alt="blog image" />
+      </div>
       <div>content</div>
       <div>author</div>
-    </div>
+    </main>
   );
 }
 
