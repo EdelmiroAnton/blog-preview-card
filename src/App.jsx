ecc58ab2ac44d6cbc16fc580eeb1cd83dbf8165b
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-12-22 20:08:19 +0100

    add yellow main color

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-
 function App() {
-
   return (
     <>
-    <div className="bg-red-300">
-      VITE AND TAILWIND OK!!
-    </div>
+      <div className="">
+        VITE AND TAILWIND OK!!
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
