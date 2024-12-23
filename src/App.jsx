21fc426ca07910e393c9976b4ab560db10654ced
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-12-23 18:06:44 +0100

    add date of published

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,13 @@ function App() {
           aria-label="Learning"
           className="w-[73px] h-[26px] bg-myYellow rounded-md flex justify-center items-center"
         >
-          <h3 className="font-Figtree text-center text-[12px]">Learning</h3>
+          <h3 className="font-Figtree font-extrabold text-center text-[12px]">
+            Learning
+          </h3>
         </div>
+        <p className="font-Figtree font-medium text-[12px] mt-[12px] text-myGrey">
+          Published 21 Dec 2023
+        </p>
       </section>
       <div>author</div>
     </main>
