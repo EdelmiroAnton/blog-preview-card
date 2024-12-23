c44898dc97dd294cb788a7693709038cb727b30f
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-12-23 17:48:27 +0100

    add font family in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,11 @@ export default {
         myYellow: "#F4D04E",
       },
       boxShadow: {
-        cardShadow: " 9px 9px 0 0 #000000"
-      }
+        cardShadow: "9px 9px 0 0 #000000",
+      },
+      fontFamily: {
+        Figtree: ["Figtree, serif"],
+      },
     },
   },
   plugins: [],
