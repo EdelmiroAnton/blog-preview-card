09b15e29313d512adab168f2a806226ac2f8e152
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-12-30 13:38:31 +0100

    add desktop media queries styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,42 @@
-import blog_image from "../src/assets/images/blog_image.png";
+import mobile_blog_image from "../src/assets/images/mobile_blog_image.png";
 import user_image from "../src/assets/images/image-avatar.webp";
 
 function App() {
   return (
     <main
       className="bg-white w-[327px] h-[501px] shadow-cardShadow rounded-2xl  flex flex-col 
-    sm:border-solid sm:border-black sm:border-[1px]"
+    desktop:border-solid 
+    desktop:border-black 
+    desktop:border-[1px] desktop:w-[384px] 
+    desktop:h-[522px]"
     >
-      <div className=" mt-6 mb-6 mx-auto">
+      <div className="mt-6 mb-6 mx-auto desktop:w-[336px]">
         <img
-          src={blog_image}
+          src={mobile_blog_image}
           alt="blog image"
-          className="w-[279px] h-[200px]"
+          className="w-full h-[200px]"
         />
       </div>
+
       <section
         aria-label="main content of the card"
-        className="w-[279px] h-[173px] mx-auto"
+        className="w-[279px] h-[173px] mx-auto desktop:w-[336px] "
       >
         <div
           aria-label="Learning"
-          className="w-[73px] h-[26px] bg-myYellow rounded-md flex justify-center items-center"
+          className="w-[73px] h-[26px] bg-myYellow rounded-md flex justify-center items-center desktop:w-[82px] desktop:h-[29px]"
         >
-          <h3 className="font-Figtree font-extrabold text-center text-[12px]">
+          <h3 className="font-Figtree font-extrabold text-center text-[12px] desktop:text-[14px]">
             Learning
           </h3>
         </div>
-        <p className="font-Figtree font-medium text-[12px] mt-[12px] text-myGrey">
+        <p className="font-Figtree font-medium text-[12px] mt-[12px] text-myGrey desktop:text-[14px]">
           Published 21 Dec 2023
         </p>
-        <h1 className="font-Figtree font-extrabold mt-[12px] text-[20px]">
+        <h1 className="font-Figtree font-extrabold mt-[12px] text-[20px] desktop:text-[1.5rem]">
           HTML & CSS foundations
         </h1>
-        <p className="font-Figtree font-medium text-myGreyParagraph text-sm mt-[12px]">
+        <p className="font-Figtree font-medium text-myGreyParagraph text-sm mt-[12px] desktop:text-[1rem]">
           These languages are the backbone of every website, defining structure,
           content, and presentation.
         </p>
